a795e78789d68787bc32a9bb526526f34350316f
Artem Baraniuk <artem.baranyuk@gmail.com>
2016-10-28 15:30:18 +0300

    Initial commit

---
+++ src/main/java/com/wslfinc/server/Publisher.java
@@ -0,0 +1,19 @@
+package com.wslfinc.server;
+
+import com.wslfinc.subd.SUBDService;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by Artem on 21.10.2016.
+ */
+public class Publisher {
+
+    public static void main(String... args) {
+        int port = 7777;
+        //String address = "http://0.0.0.0:" + port + "/wss/subd";
+        //Endpoint.publish(address, new SUBDService());
+         Endpoint.publish("http://localhost:1986/wss/hello", new SUBDService());
+        //System.out.println("Web server running at " + address);
+    }
+}
