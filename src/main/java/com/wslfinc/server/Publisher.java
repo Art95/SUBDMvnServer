68bd0a9676842ba7e81c71f879ffcedf96aab6f1
Artem Baraniuk <artem.baranyuk@gmail.com>
2016-10-28 19:07:47 +0300

    Added args usage in main

--- src/main/java/com/wslfinc/server/Publisher.java
+++ src/main/java/com/wslfinc/server/Publisher.java
@@ -10,10 +10,15 @@ import javax.xml.ws.Endpoint;
 public class Publisher {
 
     public static void main(String... args) {
-        int port = 7777;
-        //String address = "http://0.0.0.0:" + port + "/wss/subd";
-        //Endpoint.publish(address, new SUBDService());
-         Endpoint.publish("http://localhost:1986/wss/hello", new SUBDImpl());
-        //System.out.println("Web server running at " + address);
+        int port = 1986;
+
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        String address = "http://localhost:" + port + "/wss/subd";
+        Endpoint.publish(address, new SUBDImpl());
+         //Endpoint.publish("http://localhost:1986/wss/hello", new SUBDImpl());
+        System.out.println("Web server running at " + address);
     }
 }
